8fe7a9988065dc2e3fa3acebfb26f1748fd6b37f
Anton Babenko <anton@telenordigital.com>
2015-10-21 16:59:13 +0200

    Added codeship_setup.sh

--- codeship_setup.sh
+++ codeship_setup.sh
@@ -7,11 +7,12 @@ mkdir -p ~/cache/terraform
 
 cd ~/cache/terraform
 wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.4_linux_amd64.zip
-unzip terraform_0.6.4_linux_amd64.zip -d /usr/local/bin
+unzip terraform_0.6.4_linux_amd64.zip -d ~/bin
 cd ..
 
 ls -al ~/cache
 ls -al ~/cache/terraform
+ls -al ~/bin
 
 #echo $PATH
 
