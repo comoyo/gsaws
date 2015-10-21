94bbe040e6b71b99849e908dc8fabae6fdffe488
Anton Babenko <anton@telenordigital.com>
2015-10-21 16:41:42 +0200

    Added codeship_setup.sh

--- codeship_setup.sh
+++ codeship_setup.sh
@@ -11,7 +11,10 @@ wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.4_linux_amd64.zip
 unzip terraform_0.6.4_linux_amd64.zip
 cd ..
 
-echo $PATH
+ls -al ~/cache
+ls -al ~/cache/terraform
+
+#echo $PATH
 
 export PATH=~/cache/terraform:$PATH
 
